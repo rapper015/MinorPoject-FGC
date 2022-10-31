c477442be609059bea94e5021bffbfe09d6595d0
rapper015 <ashishoraon2309@gmail.com>
2022-11-01 01:09:14 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const nav_button = document.querySelector('.nav_button');
+const menu=document.querySelector('.menu');
 let nav_buttonopen = false;
 nav_button.addEventListener('click', () => {
 	if (!nav_buttonopen) {
 		nav_button.classList.add('open');
+		menu.classList.add('active');
 		nav_buttonopen = true;
 	} else {
 		nav_button.classList.remove('open');
+		menu.classList.remove('active');
 		nav_buttonopen = false;
 	}
 })
